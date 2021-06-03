8e284d395058557670eaff527d86516a5207c6c1
Pankaj Bairwa <bairwapankaj25@gmail.com>
2021-06-04 00:27:18 +0530

    WelcomeActivity button implemented

--- app/src/main/java/project/pansari/WelcomeActivity.java
+++ app/src/main/java/project/pansari/WelcomeActivity.java
@@ -2,7 +2,10 @@ package project.pansari;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class WelcomeActivity extends AppCompatActivity {
 
@@ -10,5 +13,21 @@ public class WelcomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+
+
+    }
+
+    public void shopkeeperButtonClick(View view){
+        startLoginActivity('s');
+    }
+
+    public void wholesalerButtonClick(View view){
+        startLoginActivity('w');
+    }
+
+    private void startLoginActivity(char type) {
+        Intent intent = new Intent(WelcomeActivity.this, LoginActivity.class);
+        intent.putExtra("type", type);
+        startActivity(intent);
     }
 }
