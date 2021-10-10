ddc087550abbeb3284e694d55b2fa3c0512efcad
Pankaj Bairwa <bairwapankaj25@gmail.com>
2021-10-10 14:36:50 +0530

    Wholesaler banners and products are now visible in shopkeeperCart.

--- app/src/main/java/project/pansari/viewHolders/CartProductHolder.java
+++ app/src/main/java/project/pansari/viewHolders/CartProductHolder.java
@@ -1,9 +1,14 @@
 package project.pansari.viewHolders;
 
+import android.view.LayoutInflater;
+import android.view.View;
+
 import androidx.recyclerview.widget.RecyclerView;
 
 import project.pansari.databinding.SingleCartProductCardBinding;
+import project.pansari.databinding.SingleCartProductItemBinding;
 import project.pansari.models.CartItem;
+import project.pansari.models.CartProduct;
 
 public class CartProductHolder extends RecyclerView.ViewHolder {
 
@@ -17,5 +22,24 @@ public class CartProductHolder extends RecyclerView.ViewHolder {
 
     public void setData(CartItem p) {
         binding.setItem(p);
+
+        binding.setListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (binding.singleCartProductsLayout.getVisibility() == View.VISIBLE) {
+                    binding.singleCartProductsLayout.setVisibility(View.GONE);
+                } else {
+                    binding.singleCartProductsLayout.setVisibility(View.VISIBLE);
+                }
+            }
+        });
+
+        for (CartProduct cartProduct : p.getProducts()) {
+
+            SingleCartProductItemBinding bind = SingleCartProductItemBinding.inflate(LayoutInflater.from(itemView.getContext()), binding.singleCartProductsLayout, false);
+            bind.setProduct(cartProduct);
+
+            binding.singleCartProductsLayout.addView(bind.getRoot());
+        }
     }
 }
